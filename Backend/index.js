8145724c7d94889d2179fb9b09808e1ce88a2e0f
Hammad498 <hammadabrar498@gmail.com>
2025-06-10 10:59:45 +0500

    projectStarted

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app=express();
+
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("server get request!")
+})
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
